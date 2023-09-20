f427a2ba87a800a731b8c9e8800ab87e87bd7003
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-09-19 21:16:53 -0400

    Refactor code to use 'useEffect' for cart  updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { UseSelector, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -7,14 +8,23 @@ import Products from "./components/Shop/Products";
 // Define the main App component.
 function App() {
   // Use the useSelector hook to access the "cartIsVisible" state from the Redux store.
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible); // Get the "cartIsVisible" state from Redux.
+  const cart = useSelector((state) => state.cart); // Get the "cart" state from Redux.
+
+  // Use useEffect to send a PUT request to update the cart data on a remote server when the "cart" state changes.
+  useEffect(() => {
+    fetch("https://react-http-9b836-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart), // Convert the cart data to JSON and send it in the request body.
+    });
+  }, [cart]); // Run this effect whenever the "cart" state changes.
 
   // Render the App component within a Layout, conditionally rendering the Cart component based on the "showCart" value.
   return (
     <Layout>
       {showCart && <Cart />}{" "}
       {/* Render the Cart component if "showCart" is true. */}
-      <Products /> {/* Always render the Products component. */}
+      <Products />
     </Layout>
   );
 }
