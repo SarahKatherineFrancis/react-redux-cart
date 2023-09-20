b3c5120160fb71b2ad96d26752293bff90ded286
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-09-20 16:59:48 -0400

    Refactor App component and cart management

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 // Define a flag to track the initial render.
 let isInitial = true;
@@ -16,63 +16,20 @@ function App() {
   const dispatch = useDispatch();
 
   // Access various pieces of state from the Redux store.
-  const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart = useSelector((state) => state.cart);
-  const notification = useSelector((state) => state.ui.notification);
+  const showCart = useSelector((state) => state.ui.cartIsVisible); // Get the cart visibility state.
+  const cart = useSelector((state) => state.cart); // Get the cart data.
+  const notification = useSelector((state) => state.ui.notification); // Get notification data.
 
   // Use useEffect to handle cart data updates.
   useEffect(() => {
-    // Function to send cart data to a remote server.
-    const sendCartData = async () => {
-      // Show a pending notification.
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-
-      // Send a PUT request to update cart data on the server.
-      const response = await fetch(
-        "https://react-http-9b836-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      // Check if the request was successful.
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      // Show a success notification.
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
-    // Skip the initial render.
+    // Skip the initial render where isInitial is true.
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    // Call the sendCartData function and handle errors with notifications.
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    // Dispatch an action to send cart data to the server when the cart state changes.
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]); // Run this effect whenever the "cart" state changes.
 
   // Render the App component within a Layout, conditionally rendering the Cart component based on the "showCart" value.
@@ -87,8 +44,8 @@ function App() {
         />
       )}
       <Layout>
-        {showCart && <Cart />}{" "}
         {/* Render the Cart component if "showCart" is true. */}
+        {showCart && <Cart />} {/* Conditionally render the Cart component. */}
         <Products />
       </Layout>
     </>
